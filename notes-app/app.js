d9d16b4a7be4df0a0aff29d0cc6aa40df033937b
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-18 18:17:48 +0530

    Importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
-
+const validator = require("validator");
 const { name, add } = require("./utils.js");
 
 const sum = add(2, 3);
 
+const yourNotes = require("./notes.js");
+
 console.log(name);
 console.log(sum);
+console.log(yourNotes());
+console.log(validator.isEmail("abc@gmail.com"));
