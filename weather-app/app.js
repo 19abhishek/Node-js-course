bce5ee5368c934086732ee97e59deca78ade9b24
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-23 11:14:16 +0530

    Error handling for invalid requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,16 +6,24 @@ const url =
 request({ url, json: true }, (error, response) => {
   if (error) {
     console.log("There was an error in fetching the data");
+  } else if (response.body.error) {
+    console.log("Unable to find location");
   } else {
     console.log(response.body.current);
   }
 });
 
 const geocodeURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJoaXNoZWswODE5IiwiYSI6ImNreXFxMzR6YzBtMHoydm12cWNmenA3ZjcifQ.MXOH-k1EnjqQIb4dnrAXWg";
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Dhanbad.json?access_token=pk.eyJ1IjoiYWJoaXNoZWswODE5IiwiYSI6ImNreXFxMzR6YzBtMHoydm12cWNmenA3ZjcifQ.MXOH-k1EnjqQIb4dnrAXWg";
 
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[1];
-  const longitude = response.body.features[0].center[0];
-  console.log(latitude, longitude);
+  if (error) {
+    console.log("Unable to fetch geo-location");
+  } else if (response.body.features.length === 0) {
+    console.log("Please neter a valid address");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
