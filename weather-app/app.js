435be399141004c86b3ea8355a46b69213675d0f
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-23 10:56:27 +0530

    Fetching weather data and geolocation

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,7 +3,19 @@ const request = require("request");
 const url =
   "http://api.weatherstack.com/current?access_key=1bd589b2d867b96a13d5735042dc5f94&query=New%20Delhi";
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data);
+request({ url, json: true }, (error, response) => {
+  if (error) {
+    console.log("There was an error in fetching the data");
+  } else {
+    console.log(response.body.current);
+  }
+});
+
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJoaXNoZWswODE5IiwiYSI6ImNreXFxMzR6YzBtMHoydm12cWNmenA3ZjcifQ.MXOH-k1EnjqQIb4dnrAXWg";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1];
+  const longitude = response.body.features[0].center[0];
+  console.log(latitude, longitude);
 });
