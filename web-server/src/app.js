b858c1e3de0c2f2f687bc6d928e2cb89a0969bd3
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-27 17:01:13 +0530

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,7 +7,21 @@ console.log(__filename);
 const app = express();
 const publicPath = path.join(__dirname, "../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    author: "Abhishek",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    author: "Abhishek",
+  });
+});
 
 // app.get("/help", (req, res) => {
 //   res.send({
@@ -16,13 +30,13 @@ app.use(express.static(publicPath));
 //   });
 // });
 
-app.get("/about", (req, res) => {
-  res.send("About Page.");
-});
+// app.get("/about", (req, res) => {
+//   res.send("About Page.");
+// });
 app.get("/weather", (req, res) => {
   res.send("Weather Page.");
 });
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log("Server running on port 3000.");
 });
