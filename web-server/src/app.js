8b09a735f1808d271236ea5b818653390d10f943
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-27 22:03:51 +0530

    Advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,37 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 console.log(__dirname);
 console.log(__filename);
 
 const app = express();
-const publicPath = path.join(__dirname, "../public");
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
-app.use(express.static(publicPath));
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
     author: "Abhishek",
+    name: "Nik",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     author: "Abhishek",
+    name: "Nik",
   });
 });
 
@@ -33,6 +45,7 @@ app.get("/about", (req, res) => {
 // app.get("/about", (req, res) => {
 //   res.send("About Page.");
 // });
+
 app.get("/weather", (req, res) => {
   res.send("Weather Page.");
 });
